e68011397124c79b0d1754121ab38d7184dc4ecc
Tamer Ahmed <tamer30230@gmail.com>
2023-10-20 19:40:23 +1100

    show issue details

---
+++ app/issues/new/loading.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LoadNewIssuePage = () => {
+  return (
+    <div>
+      Loading...
+    </div>
+  )
+}
+
+export default LoadNewIssuePage
--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -5,8 +5,10 @@ import React from 'react';
 import IssueStatusBadge from '../components/IssueStatusBadge';
 import delay from 'delay';
 import IssueActions from './IssueActions';
+import { notFound } from 'next/navigation';
 
 const IssuesPage = async () => {
+	
 	const issues = await prisma.issue.findMany();
   await delay(2000);
 	return (
@@ -28,7 +30,10 @@ const IssuesPage = async () => {
 					{issues.map((issue) => (
 						<Table.Row key={issue.id}>
 							<Table.Cell>
+								<Link href={`/issues/${issue.id}`}>
 								{issue.title}
+								
+								</Link>
 								<div className='block md:hidden'><IssueStatusBadge status={issue.status}/></div>
 							</Table.Cell>
 							<Table.Cell className='hidden md:table-cell'>
