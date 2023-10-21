824ecbf25c4be5ef7e50c1584799fba545e6c2f0
Tamer Ahmed <tamer30230@gmail.com>
2023-10-21 23:39:35 +1100

    Add the edit button

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,9 +1,7 @@
 import prisma from '@/prisma/migrations/client';
 import { Button, Table } from '@radix-ui/themes';
-// import Link from 'next/link';
 import React from 'react';
 import IssueStatusBadge from '../components/IssueStatusBadge';
-import delay from 'delay';
 import IssueActions from './IssueActions';
 import { notFound } from 'next/navigation';
 import Link from '../components/Link';
@@ -11,7 +9,7 @@ import Link from '../components/Link';
 const IssuesPage = async () => {
 	
 	const issues = await prisma.issue.findMany();
-  await delay(2000);
+ 
 	return (
 		<div>
 			<IssueActions />
