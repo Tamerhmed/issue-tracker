c8ddfde43a87bb4aa9a5ad8340484fbb6f5a9077
Tamer Ahmed <tamer30230@gmail.com>
2023-10-26 20:34:07 +1100

    Delete an issue

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,32 +1,53 @@
-import { issueSchema } from "@/app/validationSchema";
-import prisma from "@/prisma/migrations/client";
-import { NextRequest, NextResponse } from "next/server";
+import { issueSchema } from '@/app/validationSchema';
+import prisma from '@/prisma/migrations/client';
+import { NextRequest, NextResponse } from 'next/server';
 
-export async function PATCH(request: NextRequest, {params}: {params:{id:string}}) {
+export async function PATCH(
+	request: NextRequest,
+	{ params }: { params: { id: string } }
+) {
+	const body = await request.json();
 
-    const body = await request.json();
+	const validation = issueSchema.safeParse(body);
 
-    const validation = issueSchema.safeParse(body);
+	if (!validation.success) {
+		return NextResponse.json(validation.error.format(), { status: 400 });
+	}
 
-    if(!validation.success) {
-        return NextResponse.json(validation.error.format(), {status: 400});
-    }
+	const issue = await prisma.issue.findUnique({
+		where: { id: parseInt(params.id) },
+	});
 
+	if (!issue) {
+		return NextResponse.json({ error: 'Invalid issue' }, { status: 404 });
+	}
+
+	const updatedIssue = await prisma.issue.update({
+		where: { id: issue.id },
+		data: {
+			title: body.title,
+			description: body.description,
+		},
+	});
+
+	return NextResponse.json(updatedIssue, { status: 201 });
+}
+
+export async function DELETE(
+	request: NextRequest,
+	{ params }: { params: { id: string } }
+) {
     const issue = await prisma.issue.findUnique({
-        where: {id: parseInt(params.id)}
-    });
-
-    if(!issue){
-        return NextResponse.json({error: 'Invalid issue'}, {status:404});
-    }
-
-    const updatedIssue = await prisma.issue.update({
-        where: {id: issue.id},
-        data: {
-            title: body.title,
-            description:body.description
-        }
+        where:{id: parseInt(params.id)}
     })
 
-    return NextResponse.json(updatedIssue, {status: 201})
+    if(!issue){
+        return NextResponse.json({error:'Invalid Issue'}, {status:404});
+    }
+
+    await prisma.issue.delete({
+        where:{id: issue.id}
+    });
+
+    return NextResponse.json({});
 }
--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,12 +1,41 @@
-import { Button } from '@radix-ui/themes'
+'use client';
 
+import { AlertDialog, Button, Flex } from '@radix-ui/themes';
+import axios from 'axios';
+import { useRouter } from 'next/navigation';
 
-const DeleteIssueButton = ({issueId}: {issueId: number}) => {
-  return (
-   <Button color='red'>
-    Delete Issue
-   </Button>
-  )
-}
+const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
+    const router = useRouter();
 
-export default DeleteIssueButton
+    const deleteIssue = async ()=> {
+        await axios.delete(`/api/issues/${issueId}`);
+        router.push('/issues');
+        router.refresh();
+    }
+
+	return (
+		<AlertDialog.Root>
+			<AlertDialog.Trigger>
+				<Button color='red'>Delete Issue</Button>
+			</AlertDialog.Trigger>
+            <AlertDialog.Content>
+                <AlertDialog.Title>Confirm Deletion</AlertDialog.Title>
+                <AlertDialog.Description>Are you sure you want to delete this issue? This action cannot be undone.</AlertDialog.Description>
+                <Flex mt='4' gap='3'>
+
+                <AlertDialog.Cancel>
+                    <Button color='gray' variant='soft'>Cancel</Button>
+                </AlertDialog.Cancel>
+                <AlertDialog.Action>
+
+                    <Button color='red' variant='soft'
+                    onClick={deleteIssue}
+                    >Delete</Button>
+                </AlertDialog.Action>
+                </Flex>
+            </AlertDialog.Content>
+		</AlertDialog.Root>
+	);
+};
+
+export default DeleteIssueButton;
